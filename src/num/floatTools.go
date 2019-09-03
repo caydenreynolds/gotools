2f957ccca7960e59023b2250df6d886f18386744
Cayden Reynolds <cayden@arclasr.net>
2019-09-02 22:39:07 -0400

    Added function to find index of maximum value in a slice of floats

--- src/num/floatTools.go
+++ src/num/floatTools.go
@@ -2,15 +2,28 @@ package num
 
 import "math"
 
+//Find value of largest float in a slice
 func FindMax(floats []float32) float32 {
-	var maximum float64
-	for i := 0; i < len(floats); i++ {
+	return floats[FindMaxIndex(floats)]
+}
+
+//Finds index of largest float in a slice
+func FindMaxIndex(floats []float32) int {
+	if len(floats) == 0 {
+		panic("Cannot find maximum index of empty slice")
+	}
+
+	maximumValue := float64(floats[0])
+	maximumIndex := 0
+
+	for i := 1; i < len(floats); i++ {
 		if i == 0 {
-			maximum = float64(floats[i])
+			maximumValue = float64(floats[i])
 		} else {
-			maximum = math.Max(maximum, float64(floats[i]))
+			maximumIndex = i
+			maximumValue = math.Max(maximumValue, float64(floats[i]))
 		}
 	}
 
-	return float32(maximum)
+	return maximumIndex
 }
