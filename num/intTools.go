d2370436cb548053858bc34bc10a56c57741ae32
Cayden Reynolds <cayden@arclasr.net>
2020-01-04 19:38:36 -0500

    Added int max function

--- num/intTools.go
+++ num/intTools.go
@@ -7,3 +7,11 @@ func Min(a, b int) int {
 		return b
 	}
 }
+
+func Max(a, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
+}
--- num/intTools_test.go
+++ num/intTools_test.go
@@ -17,3 +17,19 @@ func TestMin(t *testing.T) {
 		}
 	}
 }
+
+func TestMax(t *testing.T) {
+	a := 10
+
+	for b := 0; b < 10; b++ {
+		if Max(a, b) != a {
+			t.Errorf("Max function has failed")
+		}
+	}
+
+	for b := 11; b < 20; b++ {
+		if Max(a, b) != b {
+			t.Errorf("Max function has failed")
+		}
+	}
+}
